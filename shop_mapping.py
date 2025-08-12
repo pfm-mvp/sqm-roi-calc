1bd84afd3f8503a4cff8a569757370377ba11c7a
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-08-12 16:42:14 +0200

    Update shop_mapping.py

--- shop_mapping.py
+++ shop_mapping.py
@@ -2,7 +2,9 @@ SHOP_NAME_MAP = {
     32224: "Amersfoort",
     31977: "Amsterdam",
     31831: "Den Bosch",
+    32872: "Haarlem",
     32319: "Leiden",
+    32871: "Maastricht",
     30058: "Nijmegen",
     32320: "Rotterdam",
     32204: "Venlo"
